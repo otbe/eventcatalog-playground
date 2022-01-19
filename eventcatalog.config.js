de940c1222a8e35329b2c89afc6c17020882e491
Benjamin Otto <benjamin.otto@style-systems.de>
2022-01-19 13:59:44 +0100

    wip

--- eventcatalog.config.js
+++ eventcatalog.config.js
@@ -1,32 +1,35 @@
 module.exports = {
-  title: 'EventCatalog',
-  tagline: 'Discover, Explore and Document your Event Driven Architectures',
-  organizationName: 'Your Company',
-  projectName: 'Event Catalog',
-  editUrl: 'https://github.com/boyney123/eventcatalog-demo/edit/master',
+  title: "EventCatalog",
+  tagline: "Discover, Explore and Document your Event Driven Architectures",
+  organizationName: "Your Company",
+  projectName: "Event Catalog",
   logo: {
-    alt: 'EventCatalog Logo',
+    alt: "EventCatalog Logo",
     // found in the public dir
-    src: 'logo.svg',
+    src: "logo.svg",
   },
   footerLinks: [
-    { label: 'Events', href: '/events' },
-    { label: 'Services', href: '/services' },
-    { label: '3D Node Graph', href: '/overview' },
-    { label: 'GitHub', href: 'https://github.com/boyney123/eventcatalog-demo/edit/master' }
+    { label: "Events", href: "/events" },
+    { label: "Services", href: "/services" },
+    { label: "3D Node Graph", href: "/overview" },
+    {
+      label: "GitHub",
+      href: "https://github.com/boyney123/eventcatalog-demo/edit/master",
+    },
   ],
   users: [
     {
-      id: 'dboyne',
-      name: 'David Boyne',
-      avatarUrl: 'https://pbs.twimg.com/profile_images/1262283153563140096/DYRDqKg6_400x400.png',
-      role: 'Developer',
+      id: "dboyne",
+      name: "David Boyne",
+      avatarUrl:
+        "https://pbs.twimg.com/profile_images/1262283153563140096/DYRDqKg6_400x400.png",
+      role: "Developer",
     },
     {
-      id: 'mSmith',
-      name: 'Matthew Smith',
-      avatarUrl: 'https://randomuser.me/api/portraits/lego/3.jpg',
-      role: 'Developer',
+      id: "mSmith",
+      name: "Matthew Smith",
+      avatarUrl: "https://randomuser.me/api/portraits/lego/3.jpg",
+      role: "Developer",
     },
   ],
-}
+};
